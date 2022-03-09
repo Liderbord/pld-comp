e6c8487ab6d2a654a52ce3efa0c27443d0c737e5
M4TTRX <git@matthieu-roux.com>
2022-03-09 11:04:57 +0100

    fix: improved test 3 and fixed code so it passes

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -4,43 +4,40 @@ using namespace std;
 
 static const string START_MAC = ".globl	_main\n_main:\n";
 static const std::string START_OTHERS = ".globl	main\nmain:\n";
-static const std::string END = "\tret\n";
-
-
+static const std::string END = "# epilogue\n\tpopq\t %rbp  # restore %rbp from the stack\n\tret  # return to the caller (here the shell)\n";
 
 antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx) 
 {
 
-
-
-	cout << "#prog" << std::endl;
+	cout << "# prog" << std::endl;
 	string body;
 #ifdef __APPLE__
 	body = START_MAC;
 #else
 	body = START_OTHERS;
 #endif
-		ifccParser::ContentContext *contentContext = ctx->content();
+	ifccParser::ContentContext *contentContext = ctx->content();
+	body += "\tendbr64\n \tpushq\t%rbp  # save %rbp on the stack\n\tmovq\t%rsp, %rbp # define %rbp for the current function\n";
 
-
-
-	if (contentContext) {
-		cout << "#->content" << endl;
+	if (contentContext)
+	{
+		cout << "# ->content" << endl;
 		string content = visit(contentContext).as<string>();
 
 		body += "\t" + content + "\n";
 	}
 	string returnValue = visit(ctx->returnValue()).as<string>();
-	cout << body << "\t" << returnValue << "\n" << END;
+	cout << body << "\t" << returnValue << "\n"
+		 << END;
 	return 0;
 }
 
-antlrcpp::Any CodeGenVisitor::visitContent(ifccParser::ContentContext *ctx) 
+antlrcpp::Any CodeGenVisitor::visitContent(ifccParser::ContentContext *ctx)
 {
 
-	cout << "#content" << endl;
+	cout << "# content" << endl;
 	string init = visit(ctx->init()).as<string>();
-	cout<<"#";
+	cout << "# ";
 	cout << init << endl;
 
 	ifccParser::ContentContext * contentContext = ctx->content();
@@ -55,22 +52,22 @@ antlrcpp::Any CodeGenVisitor::visitContent(ifccParser::ContentContext *ctx)
 antlrcpp::Any CodeGenVisitor::visitInit(ifccParser::InitContext *ctx) 
 {
 
-	cout << "#init" << endl;
+	cout << "# init" << endl;
 	string type = ctx->TYPE()->getText();
-	cout<< "#";
+	cout << "# ";
 	cout << type << endl;
 	string varname = ctx->VARNAME()->getText();
 	string constval = ctx->CONST()->getText();
 
 	int index = (this->vars.size() + 1) * 8;
 	this->vars[varname] = index;
-	return "movl $" + constval + ", -" + to_string(index) + "(%rbp)";
+	return "movl\t$" + constval + ", -" + to_string(index) + "(%rbp)";
 }
 
 antlrcpp::Any CodeGenVisitor::visitReturnValue(ifccParser::ReturnValueContext *ctx) 
 {
 
-	cout << "#returnValue" << endl;
+	cout << "# returnValue" << endl;
 	string returnval;
 
 	antlr4::tree::TerminalNode * varnameNode = ctx->VARNAME();
@@ -81,5 +78,5 @@ antlrcpp::Any CodeGenVisitor::visitReturnValue(ifccParser::ReturnValueContext *c
 	} else {
 		returnval = "$" + ctx->CONST()->getText();
 	}
-	return "movl " + returnval + ", %eax";
+	return "movl\t" + returnval + ", %eax";
 }
