446fda03a56f719a0bd2910d3be75e28bd75c655
patriciobcs <38959764+patriciobcs@users.noreply.github.com>
2022-03-09 10:30:06 -0300

    feat: expression basic operations (#16)
    * feat: initialize variable
    
    * feat: add and mult expressions
    
    * feat: add and mult
    
    Co-authored-by: Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -1,15 +1,12 @@
 #include "CodeGenVisitor.h"
 using namespace std;
 
-
 static const string START_MAC = ".globl	_main\n_main:\n";
 static const std::string START_OTHERS = ".globl	main\nmain:\n";
 static const std::string END = "# epilogue\n\tpopq\t %rbp  # restore %rbp from the stack\n\tret  # return to the caller (here the shell)\n";
 
 antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx) 
 {
-
-	cout << "# prog" << std::endl;
 	string body;
 #ifdef __APPLE__
 	body = START_MAC;
@@ -19,16 +16,14 @@ antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx)
 	ifccParser::ContentContext *contentContext = ctx->content();
 	body += "\tendbr64\n \tpushq\t%rbp  # save %rbp on the stack\n\tmovq\t%rsp, %rbp # define %rbp for the current function\n";
 
-	if (contentContext)
-	{
-		cout << "# ->content" << endl;
-		string content = visit(contentContext).as<string>();
-
-		body += content + "\n";
+	ifccParser::ContentContext * contentContext = ctx->content();
+	std::cout << body << std::endl;
+	if (contentContext) {
+		std::string content = visit(contentContext).as<std::string>();
+		std::cout << "\t" << content << std::endl;
 	}
-	string returnValue = visit(ctx->returnValue()).as<string>();
-	cout << body << "\t" << returnValue << "\n"
-		 << END;
+	std::string value = visit(ctx->value()).as<std::string>();
+	std::cout << "\t" << "movl " << value << ", %eax" << std::endl << END;
 	return 0;
 }
 
@@ -51,32 +46,47 @@ antlrcpp::Any CodeGenVisitor::visitContent(ifccParser::ContentContext *ctx)
 
 antlrcpp::Any CodeGenVisitor::visitInit(ifccParser::InitContext *ctx) 
 {
-
-	cout << "# init" << endl;
-	string type = ctx->TYPE()->getText();
-	cout << "# ";
-	cout << type << endl;
-	string varname = ctx->VARNAME()->getText();
-	string constval = ctx->CONST()->getText();
-
+	std::string type = ctx->TYPE()->getText();
+	std::string varname = ctx->VARNAME()->getText();
+	std::string value = visit(ctx->expression()).as<std::string>();
 	int index = (this->vars.size() + 1) * 8;
 	this->vars[varname] = index;
-	return "\tmovl\t$" + constval + ", -" + to_string(index) + "(%rbp) # assigning " + constval + " to " + varname + "\n";
+	return "movl " + value + ", -" + std::to_string(index) + "(%rbp)";
 }
 
-antlrcpp::Any CodeGenVisitor::visitReturnValue(ifccParser::ReturnValueContext *ctx) 
+antlrcpp::Any CodeGenVisitor::visitValue(ifccParser::ValueContext *ctx) 
 {
-
-	cout << "# returnValue" << endl;
-	string returnval;
-
+	std::string returnval;
 	antlr4::tree::TerminalNode * varnameNode = ctx->VARNAME();
 	if (varnameNode) {
-		string varname = varnameNode->getText();
-		string index = to_string(this->vars[varname]);
+		std::string varname = varnameNode->getText();
+		std::string index = std::to_string(this->vars[varname]);
 		returnval = "-" + index + "(%rbp)";
 	} else {
 		returnval = "$" + ctx->CONST()->getText();
 	}
-	return "movl\t" + returnval + ", %eax";
+	return returnval;
+}
+
+antlrcpp::Any CodeGenVisitor::visitExpressionMult(ifccParser::ExpressionMultContext *ctx) 
+{
+	std::string leftval = visit(ctx->expression(0)).as<std::string>();
+	std::cout << "\tmovl	" << leftval << ", %eax" << std::endl;
+	std::string rightval = visit(ctx->expression(1)).as<std::string>();
+  std::cout << "\timull	" << rightval << ", %eax" << std::endl;
+	return std::string("%eax");
+}
+
+antlrcpp::Any CodeGenVisitor::visitExpressionAdd(ifccParser::ExpressionAddContext *ctx) 
+{
+	std::string leftval = visit(ctx->expression(0)).as<std::string>();
+	std::cout << "\tmovl	" << leftval << ", %eax" << std::endl;
+	std::string rightval = visit(ctx->expression(1)).as<std::string>();
+  std::cout << "\tiaddl	" << rightval << ", %eax" << std::endl;
+	return std::string("%eax");
+}
+
+antlrcpp::Any CodeGenVisitor::visitExpressionValue(ifccParser::ExpressionValueContext *ctx) 
+{
+	return visit(ctx->value()).as<std::string>();
 }
